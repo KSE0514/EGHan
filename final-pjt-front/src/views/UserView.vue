6e1c1e999b7569018ab11cad819bfff670422b8b
gahyoenj <rkgus678@gmail.com>
2024-05-20 20:51:45 +0900

    userview 파일변경

---
+++ final-pjt-front/src/views/UserView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>UserInfo</h1>
+    </div>
+    <UserInfoView />
+  </template>
+  
+  <script setup>
+  import UserInfoView from '@/components/UserInfo.vue'
+  </script>
+  
+  <style scoped>
+  
+  </style>
